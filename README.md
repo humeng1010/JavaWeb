c63dcd990743c496b97222d03f845daa4fbb3553
redyouzi <1589029587@qq.com>
2022-08-13 18:23:19 +0800

    JavaWeb review day01

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 #### 介绍
 JavaWeb的学习笔记
+
+视频地址
+
+https://www.bilibili.com/video/BV1Qf4y1T7Hx
+
+![image-20220813130646840](https://raw.githubusercontent.com/redyouzi/images-for-blog/main/img02/202208131306948.png)
+
+![image-20220813131109859](https://raw.githubusercontent.com/redyouzi/images-for-blog/main/img02/202208131311922.png)
+
--- code/d1_JDBC/src/com/JDBC/JDBCDemo1.java
+++ code/d1_JDBC/src/com/JDBC/JDBCDemo1.java
@@ -2,6 +2,7 @@ package com.JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
@@ -10,22 +11,24 @@ import java.sql.Statement;
 public class JDBCDemo1 {
     public static void main(String[] args) throws Exception {
         //1、注册驱动
-        //Class.forName("com.mysql.cj.jdbc.Driver");MySQL5之后就可以不用写这行注册驱动代码了
+        Class.forName("com.mysql.cj.jdbc.Driver");// MySQL5之后就可以不用写这行注册驱动代码了
 
         //2、获取连接
-        String url = "jdbc:mysql://localhost:3306/db2";
+//        String url = "jdbc:mysql://localhost:3306/db1?serverTimezone=UTC&characterEncoding=utf8&useUnicode=true&useSSL=false";
+        String url = "jdbc:mysql://localhost:3306/db1";
         String username = "root";
         String password = "12345678";
         Connection connection = DriverManager.getConnection(url, username, password);
 
         //3、定义sql语句
-        String sql = "update emp set age = 22 where id = 1";
+        String sql = "update db1.emp set age = 30 where id = 1 ;";
         //4、获取执行sql的对象 Statement
         Statement statement = connection.createStatement();
         //5、执行sql
         int count = statement.executeUpdate(sql);
         //6、处理结果
         System.out.println("受影响的行数：" + count);
+
 
         //7、释放资源
         statement.close();
